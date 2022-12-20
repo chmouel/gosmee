9694e21b6ad05aa44d5602e01f0f675fb085d523
Chmouel Boudjnah <chmouel@chmouel.com>
2022-12-20 09:03:22 +0100

    update templates for script
    Signed-off-by: Chmouel Boudjnah <chmouel@chmouel.com>

--- gosmee/templates/replay_script.tmpl.bash
+++ gosmee/templates/replay_script.tmpl.bash
@@ -1,9 +1,11 @@
 #!/bin/bash
+# Replay script with headers and json to the target controller
+# You can switch the targetURL to another one with the -l switch, i.e: your local debugge
+# It default to http://localhost:808
+# you can customze this target with the env variable GOSMEE_DEBUG_SERVICE
 set -euxf
 cd $(dirname $(readlink -f $0))
 
 targetURL="{{.TargetURL}}"
-## You can switch between your local debugger and your target service with -l
-## default to http://localhost:8080 unless you specify a env variable GOSEC_DEBUG_URL
 [[ ${1:-""} == -l ]] && targetURL=${GOSMEE_DEBUG_SERVICE:-"http://localhost:8080"}
 curl -H "Content-Type: {{ .ContentType }}" {{.Headers}} -X POST -d @./{{ .FileBase }}.json ${targetURL}
