be26c3fad9fc6d8e5fab09c30212754c63988085
Chmouel Boudjnah <chmouel@chmouel.com>
2023-11-14 14:56:14 +0100

    log.Fatal on errors instead of just print

--- main.go
+++ main.go
@@ -1,15 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"log"
 	"os"
 
 	gosmee "github.com/chmouel/gosmee/gosmee"
-	"github.com/mgutz/ansi"
 )
 
 func main() {
 	if err := gosmee.Run(os.Args); err != nil {
-		fmt.Fprintf(os.Stdout, "%s gosmee %s\n", ansi.Color("ERROR", "red+b"), err.Error())
+		log.Fatal(err)
 	}
 }
